da94cebcc7e23bef0c435451f725624c42a1e621
Matty Collins <matty.collins@isobar.com>
2017-08-10 16:43:14 +1000

    add - transaction limit to withdraw method

--- BankAccount.js
+++ BankAccount.js
@@ -4,13 +4,19 @@
  */
 const BankAccount = function() {
   var balance = 0;
+  const transactionLimit = 500;
 
   this.deposit = amount => {
     return balance += amount;
   };
 
   this.withdraw = amount => {
-    return balance -= amount;
+    if (amount <= transactionLimit) {
+      return balance -= amount;
+
+    } else {
+      // @todo: throw TransactionError('over transaction limit')
+    }
   };
 
   this.getBalance = () => {
