3b6d114fa4ac5fdf877755c27598d3de496353b8
Matty Collins <matty.collins@isobar.com>
2017-08-10 15:11:51 +1000

    add - unit tests for BankAccount.deposit method

--- BankAccount.spec.js
+++ BankAccount.spec.js
@@ -2,13 +2,16 @@ const BankAccount = require('./BankAccount');
 
 describe('a BankAccount', () => {
 
-  // remove the "x" from "xtest" to make this test run
-  xtest('should allow deposits', () => {
+  test('should allow deposits', () => {
     // Arrange - all necessary preconditions and inputs
+    var bankAccount = new BankAccount();
 
     // Act - on the object or method under test
+    bankAccount.deposit(32);
 
     // Assert - that the expected results have occurred
+    expect(BankAccount().deposit).not.toBeUndefined();
+    expect(bankAccount.getBalance()).toBe(32);
   });
 
   // have a free passing test
